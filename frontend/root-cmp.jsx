b7fdb432abbcbdf1efe802adeb95e000520822d4
Alon Mlievski <alonmlievski@gmail.com>
2023-03-07 15:59:01 +0200

    delete unnecessary files

---
+++ frontend/root-cmp.jsx
@@ -0,0 +1,16 @@
+
+import React from "react";
+import { Routes, Route } from "react-router";
+
+export function RootCmp() {
+    return (
+      <div>
+        <main>
+          <Routes>
+
+          </Routes>
+        </main>
+        {/* <AppFooter /> */}
+      </div>
+    );
+  }
