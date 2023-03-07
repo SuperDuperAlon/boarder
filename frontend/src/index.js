61020a0e05371731cb020f2ffe993ba877df9f03
Alon Mlievski <alonmlievski@gmail.com>
2023-03-07 17:58:22 +0200

    initial layout design - workspace cmp

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 // import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { RootCmp } from './root-cmp.jsx'
-import './assets/main.scss'
+import './assets/styles/main.scss'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
