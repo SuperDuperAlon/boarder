a94dc5bbe65493ddb41f34c64be2c760f649c7d4
Alon Mlievski <alonmlievski@gmail.com>
2023-03-13 12:57:45 +0200

    user dashboard design

--- frontend/src/cmps/user-dashboard/user-dashboard-main.jsx
+++ frontend/src/cmps/user-dashboard/user-dashboard-main.jsx
@@ -1,6 +1,6 @@
-import {AiOutlineClockCircle} from "react-icons/ai"
-import {FaTrello} from "react-icons/fa"
- 
+import { AiOutlineClockCircle } from "react-icons/ai";
+import { FaTrello } from "react-icons/fa";
+
 export function UserDashboardMain() {
   const recentBoards = ["1", "2", "3", "4", "5", "6"];
   const myBoards = ["1", "2", "3", "4", "5", "6"];
@@ -10,20 +10,29 @@ export function UserDashboardMain() {
     <>
       <div>
         <div className="mar-y-40">
-          <div className="mar-b-10 fw600 fs16-lh24"> <AiOutlineClockCircle /> Recently Viewed</div>
+          <div className="mar-b-10 fw600 fs16-lh24">
+            {" "}
+            <AiOutlineClockCircle /> Recently Viewed
+          </div>
           <ul className="rect-list">
             {recentBoards.map((board) => {
               return <li className="text-white">Project #{board}</li>;
             })}
           </ul>
         </div>
-        <h1 className="mar-y-20 txt-dg1">Your Workspaces</h1>
+        <div className="mar-y-20 txt-dg1 uppercase fs16 fw600">Your Workspaces</div>
         <div className="">
-          <div className="flex  pad-y-11">
+          <div className="flex mar-b-11">
             <div className="fs16">alon's workspace</div>
-            <ul className="horizontal-list">
+            <ul className="horizontal-list mar-l-8">
               {workspaceLinks.map((link) => {
-                return <li> <FaTrello />{link}</li>;
+                return (
+                  <li>
+                    {" "}
+                    <FaTrello />
+                    {link}
+                  </li>
+                );
               })}
             </ul>
           </div>
@@ -32,6 +41,7 @@ export function UserDashboardMain() {
               {myBoards.map((board) => {
                 return <li className="text-white">board #{board}</li>;
               })}
+              <li className="rect-list create-board">Create New Board</li>
             </ul>
           </div>
         </div>
